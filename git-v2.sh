78362fa3a910865fd8c6921d79d0b84c09b282e9
Erik Dubois <erik.dubois@gmail.com>
2020-09-02 10:56:36 +0200

    checking code

--- git-v2.sh
+++ git-v2.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-#set -e
-##################################################################################################################
-# Author 	: 	Erik Dubois
-# Website 	: 	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
 
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
